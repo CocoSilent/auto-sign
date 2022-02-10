c2117387723dfdf26c754f1fa56f165052c310f5
xsl <xsl@servyou.com.cn>
2022-02-10 12:21:13 +0800

    代码优化

--- cloudfunctions/juejin/index.js
+++ cloudfunctions/juejin/index.js
@@ -1,46 +1,58 @@
 const http = require('https');
 
 async function juejin(event, context) {
-  let reulst = false;
-  let msg = '';
+    let result = false;
+    let msg = '';
 
-  const postData = JSON.stringify({
+    const postData = JSON.stringify({});
 
-  });
+    const request = new Promise((resolve, reject) => {
+        var responseBuffer = [];
+        const req = http.request({
+            host: 'api.juejin.cn',
+            path: '/growth_api/v1/check_in?aid=2608',
+            method: 'POST',
+            headers: {
+                'cookie': 'sessionid=' + '3d1dd4cf22004e4cf4b4358b736af9d4',
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(postData)
+            },
+        }, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseBuffer.push(chunk);
+            });
+            res.on('end', () => {
+                const responseData = responseBuffer.length === 1 ? responseBuffer[0] : Buffer.concat(responseBuffer);
+                resolve(responseData);
+            });
+        })
 
-  const req = http.request({
-    host: 'api.juejin.cn',
-    path: '/growth_api/v1/check_in?aid=2608',
-    method: 'POST',
-    headers: {
-      'cookie': 'sessionid='+ '3d1dd4cf22004e4cf4b4358b736af9d4',
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData)
-    },
-  }, function (res) {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-    res.on('end', () => {
-      console.log('No more data in response.');
-    });
-  })
+        req.on('error', (e) => {
+            reject(`problem with request: ${e.message}`);
+        });
 
-  req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-  });
+        req.write(postData);
+        req.end();
+    })
 
-  req.write(postData);
-  req.end();
+    await request.then(res => {
+        const body = JSON.parse(res);
+        if (body.err_msg) {
+            result = false;
+            msg = body.err_msg;
+        } else {
+            result = true;
+        }
+    }).catch(error => {
+        result = false;
+        msg = error;
+    })
 
-
-  return {
-    reulst,
-    msg
-  };
+    return {
+        result,
+        msg
+    };
 }
 
 juejin();
