9ec025d31dda6b4c70ff8dd6d202fd1964bf3905
xsl <xsl@servyou.com.cn>
2022-02-10 12:06:03 +0800

    掘金签到

--- cloudfunctions/juejin/index.js
+++ cloudfunctions/juejin/index.js
@@ -1,18 +1,21 @@
 const http = require('https');
 
-
 async function juejin(event, context) {
   let reulst = false;
   let msg = '';
 
+  const postData = JSON.stringify({
+
+  });
+
   const req = http.request({
-    // url: 'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uuid=6957681294839219724',
     host: 'api.juejin.cn',
-    port: 443,
     path: '/growth_api/v1/check_in?aid=2608',
     method: 'POST',
     headers: {
       'cookie': 'sessionid='+ '3d1dd4cf22004e4cf4b4358b736af9d4',
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData)
     },
   }, function (res) {
     console.log(`STATUS: ${res.statusCode}`);
@@ -30,6 +33,8 @@ async function juejin(event, context) {
     console.error(`problem with request: ${e.message}`);
   });
 
+  req.write(postData);
+  req.end();
 
 
   return {
