a605b878fce638a4bdb556d5f77ca0b2a3c3a4d2
xsl <xsl@servyou.com.cn>
2022-02-11 14:30:51 +0800

    代码优化

--- cloudfunctions/juejin/index.js
+++ cloudfunctions/juejin/index.js
@@ -1,4 +1,47 @@
-const http = require('https');
+const http = require('http');
+const https = require('https');
+
+function request(config) {
+    const url = config.url;
+    const method = config.method || 'GET';
+    const header = config.header;
+    const isHttpsRequest = url.startsWith('https');
+    const transport = isHttpsRequest ? https : http;
+    const host = url.split('/')[0].replace('https', '').replace('http', '');
+    let path;
+    if (url.split('/')[1]) {
+        path = '/' + url.split('/')[1];
+    }
+    return new Promise((resolve, reject) => {
+        var responseBuffer = [];
+        const req = transport.request({
+            host,
+            path,
+            method,
+            headers: {
+                'cookie': 'sessionid=' + sessionid,
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(postData)
+            },
+        }, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseBuffer.push(chunk);
+            });
+            res.on('end', () => {
+                const responseData = responseBuffer.length === 1 ? responseBuffer[0] : Buffer.concat(responseBuffer);
+                resolve(responseData);
+            });
+        })
+
+        req.on('error', (e) => {
+            reject(`problem with request: ${e.message}`);
+        });
+
+        req.write(postData);
+        req.end();
+    })
+}
 
 async function juejin(event, context) {
     const sessionid = process.env.juejin_sessionid;
@@ -11,7 +54,7 @@ async function juejin(event, context) {
 
     const request = new Promise((resolve, reject) => {
         var responseBuffer = [];
-        const req = http.request({
+        const req = https.request({
             host: 'api.juejin.cn',
             path: '/growth_api/v1/check_in?aid=' + aid,
             method: 'POST',
